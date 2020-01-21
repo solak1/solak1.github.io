878c3608f912ebf978d8f7c68a5f2e982f719895
solak1 <solak1@canisius.edu>
2020-01-20 20:10:52 -0500

    navbar

--- assets/js/navbar2.js
+++ assets/js/navbar2.js
@@ -1,5 +1,13 @@
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
+	if (document.body.clientWidth > 250) {
+		document.getElementById("mySidenav").style.width = String(document.body.clientWidth*.5) + "px";
+
+	}
+	else {
+		document.getElementById("mySidenav").style.width = String(document.body.clientWidth) + "px";
+	}
+
+  
 }
 
 function closeNav() {
