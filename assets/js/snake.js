84451111645e6f74d0b4dd4f6e575ac589ad385e
solak1 <solak1@canisius.edu>
2020-01-26 13:27:33 -0500

    Snake Length++ Updates

--- assets/js/snake.js
+++ assets/js/snake.js
@@ -1,6 +1,6 @@
 	// Grid is 10x10 with 40 pixels per grid
-	const dim = 400;
-	const units = 20; // number of grid squards (10x10)
+	const dim = 400; // dimension
+	const units = 20; // number of grid squards (10x10) ??
 	const body_size = units - 2;
 	const grid = dim/units;
 	// canvas setup
@@ -19,6 +19,8 @@
 		constructor(init_x, init_y, px_size, color) {
 			this.x = init_x;
 			this.y = init_y;
+			this.body = [[this.x, this.y], [(this.x-20), (this.y - 20)], [(this.x-40), (this.y - 40)]];
+			this.body_len = this.body.length;
 			this.px_size = px_size;
 			this.color = color;
 			this.draw();
@@ -34,9 +36,29 @@
 		draw() {
 			ctx.strokeStyle = "black";
 			ctx.fillStyle = this.color;
-			ctx.strokeRect(this.x, this.y, grid, grid);
-			ctx.fillRect(this.x, this.y, grid, grid);
+			// this should be a for loop to draw across the list
+			var i;
+			for (i = 0; i < this.body.length; i++) {
+			  	ctx.strokeRect(this.body[i][0], this.body[i][1], grid, grid);
+				ctx.fillRect(this.body[i][0], this.body[i][1], grid, grid);
+			}
+			// ctx.strokeRect(this.x, this.y, grid, grid);
+			// ctx.fillRect(this.x, this.y, grid, grid);
 		}
+		/*
+		collide() {
+			var search_array = this.body
+			search_array = search_array.splice(1, (search_array.length)) 
+			if (search_array.find(function (this.body[0]); == undefined ){
+				this.body = [];
+				this.body_len = 0;
+			}
+			var i;
+			for (i = 1; i < cars.length; i++) {
+			  text += cars[i] + "<br>";
+			}	
+		}
+		*/
 
 		update() {
 			if (this.direction == 1) {
@@ -64,22 +86,61 @@
 					this.y = 0;
 				}
 			}
-
+			this.body.unshift([this.x,this.y]); // add new cordinates to array
+			if (this.body_len < this.body.length) {
+				this.body.pop();
+			}
 			// draw object
 			this.draw();
 		}
 
 	}
 
+	class Apple {
+		constructor(init_x, init_y, px_size, color) {
+			this.x = init_x;
+			this.y = init_y;
+			this.px_size = px_size;
+			this.color = color;
+			this.draw();
+			ctx.strokeStyle = "black";
+			ctx.fillStyle = this.color;
+			// start x, start y, width, height
+			ctx.strokeRect(this.x, this.y, grid, grid);
+			ctx.fillRect(this.x, this.y, grid, grid);
+		}
+		draw() {
+			ctx.strokeStyle = "black";
+			ctx.fillStyle = this.color;
+			ctx.strokeRect(this.x, this.y, grid, grid);
+			ctx.fillRect(this.x, this.y, grid, grid);
+		}
+
+		update() {
+			this.draw();
+		}
+	}
+
 	function clearCanvas() {
 		ctx.fillStyle = "white";
 		ctx.strokeStyle = "black";
-
 		ctx.fillRect(0,0, dim, dim)
 		ctx.strokeRect(0,0, dim, dim)
 	}
 
-	function updateScore() {
+	function updateScore(player, apple) {
+		if (player.x == apple.x && player.y == apple.y){
+				player.score += 1;
+				player.body_len += 1;
+				random_x = Math.floor(Math.random() * 10) * 20; // new x cord int 0-9 * 20
+				random_y = Math.floor(Math.random() * 10) * 20; // new y cord 
+				apple.x = random_x;
+				apple.y = random_y;
+		}
+		else {
+			apple.update();
+		}
+
 		scoreboard.innerHTML = theplayer.score;
 	}
 
@@ -87,13 +148,14 @@
 	// Game Initialization
 
 	const theplayer = new Player(dim/2, dim/2, dim/10, "green");
+	const theapple = new Apple(dim/4, dim/4, dim/10, "red");
 
 
 
 	function gameUpdate() {
 		clearCanvas();
 		theplayer.update();
-		updateScore();
+		updateScore(theplayer, theapple);
 	}
 
   window.onload = function() {
