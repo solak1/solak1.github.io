d37a45cd818f0397f51f2304f1a546c800622960
solak1 <solak1@canisius.edu>
2020-02-06 21:15:22 -0500

    Mobile Nav

---
+++ assets/js/navbarGame.js
@@ -0,0 +1,46 @@
+function openNav() {
+	if (document.body.clientWidth > 250) {
+		document.getElementById("mySidenav").style.width = String(document.body.clientWidth*.5) + "px";
+		}
+	else {
+		document.getElementById("mySidenav").style.width = String(document.body.clientWidth) + "px";
+	}
+}
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+const mobileNavButtons = document.getElementsByClassName("sideNavItem");
+
+mobileNavButtons[0].addEventListener("touchstart", () => {
+    console.log("showing camp.")
+    fiveSections[0].style.display = "block";
+    fiveSections[1].style.display = "none";
+    fiveSections[2].style.display = "none";
+    fiveSections[3].style.display = "none";
+    fiveSections[4].style.display = "none";
+});
+
+mobileNavButtons[1].addEventListener("click", () => {
+    fiveSections[0].style.display = "none";
+    fiveSections[1].style.display = "block";
+    fiveSections[2].style.display = "none";
+    fiveSections[3].style.display = "none";
+    fiveSections[4].style.display = "none";
+})
+
+mobileNavButtons[2].addEventListener("click", () => {
+    fiveSections[0].style.display = "none";
+    fiveSections[1].style.display = "none";
+    fiveSections[2].style.display = "block";
+    fiveSections[3].style.display = "none";
+    fiveSections[4].style.display = "none";
+})
+
+mobileNavButtons[3].addEventListener("click", () => {
+    fiveSections[0].style.display = "none";
+    fiveSections[1].style.display = "none";
+    fiveSections[2].style.display = "none";
+    fiveSections[3].style.display = "block";
+    fiveSections[4].style.display = "none";
+})
